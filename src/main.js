4ea1e4df7b91a79b65d8127b14562bc3e26fcd7f
sangarix <denya.garkusha.12@gmail.com>
2025-07-11 18:20:39 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import mobileMenu from './js/header.js';
+import { cookiePolicy } from './js/cookiesPolicy.js';
+import swiperGallery from './js/swiperGallery.js';
+import openText from './js/openText.js';
+
+mobileMenu();
+cookiePolicy();
+swiperGallery();
+openText();
